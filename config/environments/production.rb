66438d6e51e72788f86fea057d821a64ec281bad
bimbo_junior001 <bimbo_junior001@hotmail.com>
2018-08-10 22:07:21 -0400

    Devise action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  
+  config.action_mailer.default_url_options = { host: 'https://solutis-km.herokuapp.com'}
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
