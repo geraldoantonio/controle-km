45dddd457030e627d258bf8620ef81b0a93d5448
bimbo_junior001 <bimbo_junior001@hotmail.com>
2018-08-16 23:37:48 -0400

    Update migrations adapter postgras column

--- db/migrate/20180813140223_change_function_to_be_integer_in_functionaries.rb
+++ db/migrate/20180813140223_change_function_to_be_integer_in_functionaries.rb
@@ -1,7 +1,7 @@
 class ChangeFunctionToBeIntegerInFunctionaries < ActiveRecord::Migration[5.2]
   def change
       #change_column :table_name, :column_name, :new_type
-    change_column :functionaries, :function, :integer
+    change_column :functionaries, :function, 'integer USING CAST(column_name AS integer)'
 
   end
 end
--- db/migrate/20180813142131_change_uf_to_be_integer_in_addresses.rb
+++ db/migrate/20180813142131_change_uf_to_be_integer_in_addresses.rb
@@ -1,5 +1,5 @@
 class ChangeUfToBeIntegerInAddresses < ActiveRecord::Migration[5.2]
   def change
-    change_column :addresses, :uf, :integer
+    change_column :addresses, :uf, 'integer USING CAST(column_name AS integer)'
   end
 end
