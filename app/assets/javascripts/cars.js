bc7d1e8e32e563268eddede020b9d2b5ebf9cb33
Geraldo Júnior <bimbo_junior001@hotmail.com>
2019-03-30 18:51:17 -0300

    set min attr  km_end

--- app/assets/javascripts/cars.js
+++ app/assets/javascripts/cars.js
@@ -1,5 +1,4 @@
 function getLastKm(){  
-
   var car_id = $("#displacement_car_id option:selected").val();
 
   if (car_id == null){
@@ -11,8 +10,22 @@ function getLastKm(){
       url:"/cars/" + car_id,
       dataType:"json",
       success:function(result){
-        $("#displacement_kmStart").val(result.km);
+        $("#displacement_kmStart").val(result.km); 
+        $("#displacement_kmEnd").attr('min', result.km).val(0);
       }
     })
   }
 }
+
+$(document).ready(function(){  
+
+  $('#displacement_car_id').change(function(){
+    getLastKm();
+  });
+  
+  $("#displacement_kmStart").change(function(){
+    $("#displacement_kmEnd").attr('min', $("#displacement_kmStart").val() );
+  }); 
+
+});
+
